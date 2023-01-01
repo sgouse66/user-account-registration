aaa9adcacc6fe3d963740882b3e6bf9440f5c563
sgouse66 <121743830+sgouse66@users.noreply.github.com>
2023-01-01 11:46:44 +0530

    changes

---
+++ src/main/java/com/userReg/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.userReg.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.userReg.model.User;
+
+@Repository("userRepository")
+public interface UserRepository extends CrudRepository<User, Long> {
+	 User findByEmail(String email);
+	 User findByConfirmationToken(String confirmationToken);
+}
